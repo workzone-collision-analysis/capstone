6a36cd14f17312c664364437c42461c4f321f17e
Seunggyun Han <hsg8234@naver.com>
2020-07-10 21:42:33 -0400

    add geojson files for maps
    1. map.js
    - (fix) shortSegementURL, and shstNodeURL was changed
    - (fix) radius of point was changed (4=>5)
    - (add) sources and layers for segments and short segments
    - (add) function for click event

--- dashboard/src/js/map.js
+++ dashboard/src/js/map.js
@@ -9,25 +9,53 @@ const map = new mapboxgl.Map({
     center: [-73.997482, 40.730880]
 });
 
-const shortSegmentURL = "src/data/short_segment.geojson";
-const shstNodeURL = "src/data/shst_node.geojson";
+const shortSegmentCentroidURL = "src/data/shst_short_segment_centroid.geojson";
+const shortSegmentURL = "src/data/shst_short_segment.geojson";
+const segmentURL = "src/data/shst_segment_filtered_simplified.geojson";
+const shstNodeURL = "src/data/shst_node_filtered.geojson";
 
 map.on('load', function (){
     window.setInterval(function() {
         map.getSource('shortSegment').setData(shortSegmentURL);
+        map.getSource('segment').setData(segmentURL);
+        map.getSource('shortSegmentCentroid').setData(shortSegmentCentroidURL);
         map.getSource('shstNode').setData(shstNodeURL);
     }, 2000);
 
-    map.addSource('shortSegment', { type: 'geojson', data:shortSegmentURL , 'promoteId': 'short_segment_id'});
+    map.addSource('segment', { type: 'geojson', data:segmentURL , 'promoteId': 'id'});
+    map.addSource('shortSegment', { type: 'geojson', data:shortSegmentURL , 'promoteId': 'id'});
+    map.addSource('shortSegmentCentroid', { type: 'geojson', data:shortSegmentCentroidURL , 'promoteId': 'id'});
     map.addSource('shstNode', { type: 'geojson', data:shstNodeURL , 'promoteId': 'node_id'});
+
+    map.addLayer({
+        "id": "segment",
+        "source": "segment",
+        "type": "line",
+        "paint": {
+            "line-color": '#424242',
+            "line-width": 2
+        }
+    });
+
 
     map.addLayer({
         "id": "shortSegment",
         "source": "shortSegment",
+        "type": "line",
+        "paint": {
+            "line-color": '#424242',
+            "line-dasharray": [2,2],
+            "line-width": 1
+        }
+    });
+
+    map.addLayer({
+        "id": "shortSegmentCentroid",
+        "source": "shortSegmentCentroid",
         "type": "circle",
         "paint": {
             "circle-color": "#F44336",
-            "circle-radius":4
+            "circle-radius":5
         }
     });
 
@@ -37,8 +65,14 @@ map.on('load', function (){
         "type": "circle",
         "paint": {
             "circle-color": "#2196F3",
-            "circle-radius":4
+            "circle-radius":5
         }
     });
 
+
+    map.on("click", "shortSegment", function(e) {
+        let clicked_feature = e.features[0];
+        console.log(clicked_feature);
+    });
 });
+
